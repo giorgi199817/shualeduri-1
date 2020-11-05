e5d787fb78e63dbc222207d20d9b9c45128702f6
giorgi199817 <chanadiri24@gmail.com>
2020-11-05 10:08:19 -0800

    shualeduri 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> chatbot = new HashMap<>();
+        System.out.println("მოგესალმებით");
+
+        Scanner scanner = new Scanner(System.in );
+        System.out.println(chatbot.get(scanner.next()));
+        System.out.println("ნახვამდის");
+
+
+    }}
+
